297088f898b0b2454e1f8a541b7725010a095eb4
Jason Arnold <jason.arnold.00@gmail.com>
2017-06-17 18:02:02 -0400

    changed classes to functions for Home and Nav, changed button sytle in Home

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,27 +1,17 @@
-import React, {Component} from 'react';
-import {Route, Link} from 'react-router-dom';
+import React from 'react';
+import {Link} from 'react-router-dom';
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-
-    };
-  }
-
-  render() {
-    return (
-      <div className='home'>
-        <h1>Get info about your favorite TV shows.</h1>
-        <h3>Get a list of popular shows, or search for new ones.</h3>
-        <div className='buttons'>
-          <Link to='/popular'><button>Popular</button></Link>
-          <Link to='/search'><button>Search</button></Link>
-        </div>
+const Home = () => {
+  return (
+    <div className='home'>
+      <h1>Get info about your favorite TV shows.</h1>
+      <h3>Get a list of popular shows, or search for new ones.</h3>
+      <div className='buttons'>
+        <Link to='/popular'><button>Popular</button></Link>
+        <Link to='/search'><button>Search</button></Link>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 module.exports = Home;
--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,36 +1,26 @@
-import React, {Component} from 'react';
+import React  from 'react';
 import {NavLink} from 'react-router-dom';
 
-class Nav extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-
-    };
-  }
-
-  render() {
-    return (
-      <ul className='nav'>
-        <li>
-          <NavLink exact activeClassName='active' to='/'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink activeClassName='active' to='/popular'>
-            Popular
-          </NavLink>
-        </li>
-        <li>
-          <NavLink activeClassName='active' to='/search'>
-            Search
-          </NavLink>
-        </li>
-      </ul>
-    )
-  }
+const Nav = () => {
+  return (
+    <ul className='nav'>
+      <li>
+        <NavLink exact activeClassName='active' to='/'>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName='active' to='/popular'>
+          Popular
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName='active' to='/search'>
+          Search
+        </NavLink>
+      </li>
+    </ul>
+  )
 }
 
 module.exports = Nav;
