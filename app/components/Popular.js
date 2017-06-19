852432a102bcc35fa1dbea0899035d570445235a
Jason Arnold <jason.arnold.00@gmail.com>
2017-06-18 22:08:17 -0400

    rendering popular shows

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -33,10 +33,23 @@ class Popular extends Component {
 }
 
 const ShowGrid = (props) => {
+  let baseUrl = 'https://image.tmdb.org/t/p/w640';
   return (
-    <ul>
-      {props.shows.map((show, index) => {
-         return <li>{show.name}</li>
+    <ul className='popular-list'>
+      {props.shows.map((show) => {
+         return (
+           <li key={show.id} className='popular-item'>
+               <ul className='popular-item-content'>
+                 <li>
+                   <a href={`https://www.themoviedb.org/tv/${show.id}`}>
+                     <img src={`${baseUrl}${show.poster_path}`}></img>
+                   </a>
+                </li>
+                 <li className='showName'>{show.name}</li>
+                 <li className='showPopularity'>{show.popularity}</li>
+               </ul>
+           </li>
+         )
       })}
     </ul>
   )
